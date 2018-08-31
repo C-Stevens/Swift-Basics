a0f9bd2cdcc84d945b9e6415a29eb7dd58ae8ac9
Colin Stevens <colin@colinjstevens.com>
2018-08-31 17:28:31 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Colin Stevens on 8/31/18.
+//  Copyright © 2018 Colin Stevens. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
